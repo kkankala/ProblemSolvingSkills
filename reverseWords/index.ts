a69745efdbe6e1dd8052a9390a4a7f3567d10775
Kalyan Kankala <kankala.kalyan@gmail.com>
2022-04-16 15:06:23 -0400

    reverse words variation

--- reverseWords/index.ts
+++ reverseWords/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Given an input string s, reverse the order of the words.
+ * A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+ * Return a string of the words in reverse order concatenated by a single space.
+ * @param s - input string
+ * @returns - a string of the words in reverse order concatenated by a single space.
+ */
 const reverseWords = (s: string): string => {
   s = s.trim();
   let words = s.split(/\s+/); //remove and split any extra spaces
