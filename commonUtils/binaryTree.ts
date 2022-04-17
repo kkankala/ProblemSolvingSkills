396820a775cf4b57f10140531604339c163d6afc
Kalyan Kankala <kankala.kalyan@gmail.com>
2022-04-17 16:19:51 -0400

    level Order Traversel

--- commonUtils/binaryTree.ts
+++ commonUtils/binaryTree.ts
@@ -14,6 +14,17 @@ export class BinaryTreeNode<T> {
   }
 }
 
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 export class BinaryTree<T> {
   root: BinaryTreeNode<T>;
   constructor() {
--- microsoft/tree-and-graphs/validateBst/index.ts
+++ microsoft/tree-and-graphs/validateBst/index.ts
@@ -1,13 +1,4 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+import { TreeNode } from '../../../commonUtils/binaryTree';
 
 /**
  * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
